e79debc6540b4692c2ae362d7cae2582d6153ab7
CodeWithSubaru <francis.cloydm@gmail.com>
2023-04-20 12:52:29 +0800

    update

--- dealease-fe/src/App.jsx
+++ dealease-fe/src/App.jsx
@@ -19,6 +19,7 @@ import { DonationBuyer } from './Pages/Buyer/Donation';
 // Seller Components
 import { AuthSellerLayout } from './Layouts/AuthSellerLayout';
 import { ChangePasswordSeller } from './Pages/Seller/ChangePassword';
+import { ShopSeller } from './Pages/Seller/Shop';
 import { HomeSeller } from './Pages/Seller/Home';
 import { InboxSeller } from './Pages/Seller/Inbox';
 import { MessageSeller } from './Pages/Seller/Message';
@@ -71,6 +72,7 @@ function App() {
           {/* Seller Route */}
           <Route element={<AuthSellerLayout />}>
             <Route path='/seller/home' element={<HomeSeller />} />
+            <Route path='/seller/shop' element={<ShopSeller />} />
             <Route path='/seller/inbox' element={<InboxSeller />} />
             <Route
               path='/seller/message/:message_id'
