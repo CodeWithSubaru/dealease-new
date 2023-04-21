319f502ac28bdc0636d8092e3a010601899d656b
CodeWithSubaru <francis.cloydm@gmail.com>
2023-04-21 23:08:32 +0800

    Inbox Implementation

--- dealease-fe/src/Pages/Buyer/Inbox.jsx
+++ dealease-fe/src/Pages/Buyer/Inbox.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import useMessageContext from '../../Hooks/Context/MessageContext';
 import useAuthContext from '../../Hooks/Context/AuthContext';
 import { Button, Row, Col, Container, Badge, Form } from 'react-bootstrap';
@@ -6,11 +6,14 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowAltCircleLeft } from '@fortawesome/free-solid-svg-icons';
 import { Footer } from '../../Components/Footer/footer';
 import '../../assets/scss/inbox.scss';
+import axiosClient from '../../api/axios';
+import PUBLIC_PATH from '../../api/public_url';
 
 export const InboxBuyer = () => {
   const { user } = useAuthContext();
   const { softDel, inboxes, clickedUser, fetchInbox, messages } =
     useMessageContext();
+  const [availableUsers, setAvailableUsers] = useState([]);
 
   function dateFormat(date) {
     const convertedDate = new Date(date);
@@ -28,6 +31,10 @@ export const InboxBuyer = () => {
   }
 
   useEffect(() => {
+    axiosClient.get('/users').then((res) => {
+      console.log(res.data.listOfUser);
+      setAvailableUsers(res.data.listOfUser);
+    });
     fetchInbox();
     const chatInterval = setInterval(() => fetchInbox(), 10000);
     return () => clearInterval(chatInterval);
@@ -35,7 +42,7 @@ export const InboxBuyer = () => {
 
   return (
     <>
-      <div className='inbox-container'>
+      <div className='inbox-container' style={{ height: '80vh' }}>
         <div className='inbox-header'>
           <Container>
             <Row>
@@ -59,59 +66,96 @@ export const InboxBuyer = () => {
             </Row>
           </Container>
         </div>
-
-        {inboxes.length > 0 ? (
-          inboxes.map((inbox) => (
-            <Container className='inbox-content' key={inbox.inbox_id}>
+        <div className='position-relative w-100 h-100 d-flex flex-column text-light'>
+          <div
+            className='bg-dark w-25 h-100 position-absolute overflow-auto'
+            style={{ top: '0', left: '0' }}
+          >
+            <div>
+              <h3 className='p-3'>List of Users</h3>
+              <div className=''>
+                {availableUsers.map((user) => (
+                  <>
+                    <hr />
+                    <div className='d-flex ms-3'>
+                      <img
+                        src={PUBLIC_PATH + 'images/' + user.prof_img}
+                        alt={user.prof_img}
+                        className='rounded-circle h-25 w-25 me-2'
+                      />
+                      <div className='flex-grow-1 align-self-center'>
+                        <h5 className='mb-0'> {user.first_name}</h5>
+                        <p className='mb-0 badge rounded-pill text-bg-primary'>
+                          {user.is_buyer === 'Buyer'
+                            ? user.is_buyer
+                            : user.is_seller}
+                        </p>
+                      </div>
+                    </div>
+                  </>
+                ))}
+              </div>
+            </div>
+          </div>
+          {inboxes.length > 0 ? (
+            inboxes.map((inbox) => (
+              <Container className='inbox-content w-75' key={inbox.inbox_id}>
+                <Row>
+                  <Col>
+                    <div className='text-inbox-content'>
+                      <div className='fw-bold'>
+                        {inbox.user_id === user.user_id
+                          ? inbox.recipient.first_name
+                          : inbox.sender.first_name}{' '}
+                      </div>
+                      {inbox.last_message.chat}{' '}
+                      {dateFormat(inbox.last_message.created_at)}
+                    </div>
+                  </Col>
+                  <Col>
+                    <div className='btn-inbox-content'>
+                      <Button className='btn-content' size='sm'>
+                        Profile
+                      </Button>
+                      <Button
+                        className='btn-content'
+                        key={inbox.inbox_id}
+                        onClick={() =>
+                          clickedUser(
+                            inbox.recipient_id === user.user_id
+                              ? inbox.user_id
+                              : inbox.recipient_id,
+                            ''
+                          )
+                        }
+                        size='sm'
+                      >
+                        Message
+                      </Button>
+                      <Button
+                        className='btn-content'
+                        size='sm'
+                        onClick={() => softDel(inbox.inbox_id)}
+                      >
+                        Delete
+                      </Button>
+                    </div>
+                  </Col>
+                </Row>
+              </Container>
+            ))
+          ) : (
+            <Container className='h-100 w-100'>
               <Row>
                 <Col>
-                  <div className='text-inbox-content'>
-                    <div className='fw-bold'>
-                      {inbox.user_id === user.user_id
-                        ? inbox.recipient.first_name
-                        : inbox.sender.first_name}{' '}
-                    </div>
-                    {inbox.last_message.chat}{' '}
-                    {dateFormat(inbox.last_message.created_at)}
-                  </div>
-                </Col>
-                <Col>
-                  <div className='btn-inbox-content'>
-                    <Button className='btn-content' size='sm'>
-                      Profile
-                    </Button>
-                    <Button
-                      className='btn-content'
-                      key={inbox.inbox_id}
-                      onClick={() =>
-                        clickedUser(
-                          inbox.recipient_id === user.user_id
-                            ? inbox.user_id
-                            : inbox.recipient_id,
-                          ''
-                        )
-                      }
-                      size='sm'
-                    >
-                      Message
-                    </Button>
-                    <Button
-                      className='btn-content'
-                      size='sm'
-                      onClick={() => softDel(inbox.inbox_id)}
-                    >
-                      Delete
-                    </Button>
-                  </div>
+                  <Badge className='none-result' pill>
+                    No Message Yet
+                  </Badge>
                 </Col>
               </Row>
             </Container>
-          ))
-        ) : (
-          <Badge className='none-result' pill>
-            No Message Yet
-          </Badge>
-        )}
+          )}
+        </div>
       </div>
       <Footer />
     </>
