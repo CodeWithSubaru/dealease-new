b73770bb0c1cf4d4184bdbf7355da3f3b1164eb5
CodeWithSubaru <francis.cloydm@gmail.com>
2023-04-26 20:11:38 +0800

    update

--- dealease-fe/src/Pages/Buyer/Home.jsx
+++ dealease-fe/src/Pages/Buyer/Home.jsx
@@ -5,9 +5,17 @@ import HeroSection from '../../Components/Section/HeroSection';
 import { Card } from '../../Components/Card/Card';
 import { Footer } from '../../Components/Footer/Footer';
 import { Modal, Row, Col, Container } from 'react-bootstrap';
+import {
+  faPhone,
+  faMagnifyingGlass,
+  faHouse,
+} from '@fortawesome/free-solid-svg-icons';
+import { Sidebar, Menu, MenuItem, useProSidebar } from 'react-pro-sidebar';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export const Home = () => {
   const { user, setEmailVerified, setRegistrationSuccess } = useAuthContext();
+  const { collapseSidebar } = useProSidebar();
 
   useEffect(() => {
     return () => {
@@ -18,9 +26,34 @@ export const Home = () => {
 
   return (
     <>
-      <button className='btn btn-dark'>Recharge</button>
-      <Card />
-      <Footer />
+      <div style={{ display: 'flex', height: '100%' }}>
+        <Sidebar backgroundColor='#19a9d0'>
+          <Menu
+            menuItemStyles={{
+              button: ({ level, active, disabled }) => {
+                // only apply styles on first level elements of the tree
+                if (level === 0)
+                  return {
+                    color: disabled ? '#f5d9ff' : '#white',
+                    backgroundColor: active ? '#eecef9' : undefined,
+                  };
+              },
+            }}
+          >
+            <MenuItem>
+              <FontAwesomeIcon icon={faHouse} className='navs-icon' /> Home
+            </MenuItem>
+            <MenuItem> Products</MenuItem>
+            <MenuItem> E-commerce</MenuItem>
+          </Menu>
+        </Sidebar>
+        <main>
+          <button onClick={() => collapseSidebar()}>Collapse</button>
+          <button className='btn btn-dark'>Recharge</button>
+          <Card />
+        </main>
+        <Footer />
+      </div>
     </>
   );
 };
