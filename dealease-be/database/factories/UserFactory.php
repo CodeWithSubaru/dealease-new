c85ffd1be2ff4d8badaac13b1606e5ffb85014f9
CodeWithSubaru <francis.cloydm@gmail.com>
2023-05-02 02:19:01 +0800

    Update

--- dealease-be/database/factories/UserFactory.php
+++ dealease-be/database/factories/UserFactory.php
@@ -30,7 +30,6 @@ class UserFactory extends Factory
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'email_verified_at' => Carbon::now(),
             'user_details_id' => $increment,
-            'wallet_id' => $increment,
             'role_type' => rand(1, 2),
             'remember_token' => Str::random(10),
         ];
