9ba216cf38bab695d6f2e7890234d0516210c863
CodeWithSubaru <francis.cloydm@gmail.com>
2023-05-12 19:27:28 +0800

    Implemented Not Found Page

--- dealease-fe/src/Pages/NotFound.jsx
+++ dealease-fe/src/Pages/NotFound.jsx
@@ -1,3 +1,16 @@
 export function NotFound() {
-  return <div>NotFound</div>;
+  return (
+    <div
+      className='d-flex justify-content-center align-items-center bg-light'
+      style={{ height: '100vh' }}
+    >
+      <div className='d-flex flex-column align-items-center'>
+        <img src='./images/page/crying_fish.png' className='w-25 w-25 mb-3' />
+        <h1 className='text-secondary mb-0' style={{ fontSize: '70px' }}>
+          404
+        </h1>
+        <p> Sorry Url Not Found</p>
+      </div>
+    </div>
+  );
 }
