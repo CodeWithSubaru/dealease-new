90ee0151f96398f50e9f0198f95515c825b5e78c
CodeWithSubaru <francis.cloydm@gmail.com>
2023-05-15 00:20:39 +0800

    Added Seller name to card item

--- dealease-be/app/Models/Product.php
+++ dealease-be/app/Models/Product.php
@@ -19,8 +19,13 @@ class Product extends Model
         return $this->belongsTo(User::class, 'user_id', 'user_id');
     }
 
-    public function user()
+    public function seller()
     {
         return $this->hasOne(User::class, 'user_id', 'user_id');
+    }
+
+    public function userDetails()
+    {
+        return $this->belongsTo(UserDetail::class, 'user_detais_id', 'user_detais_id');
     }
 }
