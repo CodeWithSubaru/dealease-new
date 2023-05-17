a1b5701a003ab44703b8761816653c381231a220
CodeWithSubaru <francis.cloydm@gmail.com>
2023-05-17 16:37:02 +0800

    Update

--- dealease-fe/src/Pages/Users/Home.jsx
+++ dealease-fe/src/Pages/Users/Home.jsx
@@ -1,11 +1,11 @@
-import React, { useEffect, useState } from "react";
-import useAuthContext from "../../Hooks/Context/AuthContext";
-import { Card } from "../../Components/Card/Card";
-import { Footer } from "../../Components/Footer/Footer";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBars, faExclamationCircle } from "@fortawesome/free-solid-svg-icons";
-import Alert from "react-bootstrap/Alert";
-import { Notification } from "../../Components/Notification/Notification";
+import React, { useEffect, useState } from 'react';
+import useAuthContext from '../../Hooks/Context/AuthContext';
+import { Card } from '../../Components/Card/Card';
+import { Footer } from '../../Components/Footer/Footer';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars, faExclamationCircle } from '@fortawesome/free-solid-svg-icons';
+import Alert from 'react-bootstrap/Alert';
+import { Notification } from '../../Components/Notification/Notification';
 
 import {
   Sidebar,
@@ -14,14 +14,14 @@ import {
   SubMenu,
   useProSidebar,
   sidebarClasses,
-} from "react-pro-sidebar";
-import { Link } from "react-router-dom";
-import { Button, Modal, Row, Col } from "react-bootstrap";
-import Form from "react-bootstrap/Form";
-import useOrderContext from "../../Hooks/Context/OrderContext";
-import OverlayTrigger from "react-bootstrap/OverlayTrigger";
-import Tooltip from "react-bootstrap/Tooltip";
-import axiosClient from "../../api/axios";
+} from 'react-pro-sidebar';
+import { Link } from 'react-router-dom';
+import { Button, Modal, Row, Col } from 'react-bootstrap';
+import Form from 'react-bootstrap/Form';
+import useOrderContext from '../../Hooks/Context/OrderContext';
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
+import Tooltip from 'react-bootstrap/Tooltip';
+import axiosClient from '../../api/axios';
 
 export const HomeUser = () => {
   const [updateAccessModal, setUpdateAccessModal] = useState(false);
@@ -37,7 +37,7 @@ export const HomeUser = () => {
   const [check, setCheck] = useState(false);
   const handleCheckboxChange = (e) => {
     setCheck(e.target.checked);
-    sessionStorage.setItem("check-subs-agreement", e.target.checked);
+    sessionStorage.setItem('check-subs-agreement', e.target.checked);
   };
   const [show, setShow] = useState(false);
 
@@ -75,14 +75,14 @@ export const HomeUser = () => {
     };
 
     axiosClient
-      .post("/update-access", data, {
-        headers: { "Content-Type": "multipart/form-data" },
+      .post('/update-access', data, {
+        headers: { 'Content-Type': 'multipart/form-data' },
       })
       .then((res) => {
         Notification({
-          title: "Success",
+          title: 'Success',
           message: res.data.status,
-          icon: "success",
+          icon: 'success',
         }).then(() => {
           closeUpdateAccessModal();
         });
@@ -92,8 +92,8 @@ export const HomeUser = () => {
 
   useEffect(() => {
     setDoneTransaction(false);
-    setImgFront("../../../images/default_image.png");
-    setImgBack("../../../images/default_image.png");
+    setImgFront('../../../images/default_image.png');
+    setImgBack('../../../images/default_image.png');
     return () => {
       setRegistrationSuccess(false);
       setEmailVerified(false);
@@ -102,14 +102,14 @@ export const HomeUser = () => {
 
   return (
     <>
-      <div style={{ display: "flex", height: "100%" }}>
+      <div style={{ display: 'flex', height: '100%' }}>
         <Sidebar
-          width="190px"
-          collapsedWidth="65px"
-          transitionDuration="500"
+          width='190px'
+          collapsedWidth='65px'
+          transitionDuration='500'
           rootStyles={{
             [`.${sidebarClasses.container}`]: {
-              backgroundColor: "#19a9d0",
+              backgroundColor: '#19a9d0',
             },
           }}
         >
@@ -118,28 +118,28 @@ export const HomeUser = () => {
               button: ({ level, active, disabled }) => {
                 if (level === 0)
                   return {
-                    color: disabled ? "#f5d9ff" : "#white",
-                    backgroundColor: active ? "#eecef9" : undefined,
+                    color: disabled ? '#f5d9ff' : '#white',
+                    backgroundColor: active ? '#eecef9' : undefined,
                   };
               },
             }}
           >
-            <button className="btn " onClick={() => collapseSidebar()}>
-              <FontAwesomeIcon icon={faBars} className="navs-icon" />
+            <button className='btn ' onClick={() => collapseSidebar()}>
+              <FontAwesomeIcon icon={faBars} className='navs-icon' />
             </button>
 
-            <MenuItem className="text-black " component={<Link to="/" />}>
+            <MenuItem className='text-black ' component={<Link to='/' />}>
               Home
             </MenuItem>
 
-            <MenuItem component={<Link to="/withdraw" />}> Withdraw </MenuItem>
-            <MenuItem component={<Link to="/recharge" />}> Recharge </MenuItem>
+            <MenuItem component={<Link to='/withdraw' />}> Withdraw </MenuItem>
+            <MenuItem component={<Link to='/recharge' />}> Recharge </MenuItem>
             {user.verified_user ? (
-              <MenuItem component={<Link to="/product" />}> Product </MenuItem>
+              <MenuItem component={<Link to='/product' />}> Product </MenuItem>
             ) : (
-              <div className="d-flex flex-column justify-content-end flex-grow-1 h-100">
+              <div className='d-flex flex-column justify-content-end flex-grow-1 h-100'>
                 <Button
-                  className="btn btn-sm d-inline-block"
+                  className='btn btn-sm d-inline-block'
                   onClick={() => setUpdateAccessModal(true)}
                 >
                   Update Access
@@ -148,28 +148,28 @@ export const HomeUser = () => {
             )}
           </Menu>
         </Sidebar>
-        <main className="w-100 ">
+        <main className='w-100 '>
           <Modal
             show={updateAccessModal}
             onHide={closeUpdateAccessModal}
             animation={false}
-            size="lg"
+            size='lg'
           >
             <Modal.Header closeButton>
-              <Modal.Title className="fw-semibold">
+              <Modal.Title className='fw-semibold'>
                 Request Update Acccess
               </Modal.Title>
             </Modal.Header>
             <Modal.Body>
-              <Alert show={true} variant="success">
-                <div className="d-flex justify-content-between">
+              <Alert show={true} variant='success'>
+                <div className='d-flex justify-content-between'>
                   <Alert.Heading>Instruction </Alert.Heading>
 
                   <OverlayTrigger
-                    placement="bottom"
+                    placement='bottom'
                     overlay={
-                      <Tooltip id="button-tooltip-2">
-                        <div className="p-2 text-start">
+                      <Tooltip id='button-tooltip-2'>
+                        <div className='p-2 text-start'>
                           Once you didn't follow the instruction, we will
                           automatically reject your request.
                         </div>
@@ -179,73 +179,73 @@ export const HomeUser = () => {
                     <FontAwesomeIcon icon={faExclamationCircle} />
                   </OverlayTrigger>
                 </div>
-                <ul>
+                <ul className='ms-4'>
                   <li>
                     Please make sure that both pictures of the government IDs
                     are clear and valid.
                   </li>
                 </ul>
                 <hr />
-                <div className="clearfix">
-                  <p className="float-end mb-0"> - System Administator</p>
+                <div className='clearfix'>
+                  <p className='float-end mb-0'> - System Administator</p>
                 </div>
               </Alert>
-              <Form onSubmit={handleUpdateAccessForm} id="updateAccessForm">
-                <div className="d-flex align-items-center clearfix mb-3">
-                  <Form.Group controlId="formFile" className="mb-3 w-100">
-                    <Form.Label className="text-secondary">
-                      Valid Government Id (Front)
+              <Form onSubmit={handleUpdateAccessForm} id='updateAccessForm'>
+                <div className='d-flex align-items-center clearfix mb-3'>
+                  <Form.Group controlId='formFile' className='mb-3 w-100'>
+                    <Form.Label className='text-secondary'>
+                      First Valid Government Id
                     </Form.Label>
                     <Form.Control
-                      type="file"
-                      id="formFile"
+                      type='file'
+                      id='formFile'
                       onChange={onImageChangeFront}
                       isInvalid={!!errors.valid_id_front}
                     />
                     {console.log(errors)}
-                    <Form.Control.Feedback type="invalid">
+                    <Form.Control.Feedback type='invalid'>
                       {errors.valid_id_front}
                     </Form.Control.Feedback>
                   </Form.Group>
-                  <div className="w-50">
-                    <div className="float-end">
-                      <p className="mb-0">ID Preview (Front)</p>
+                  <div className='w-50'>
+                    <div className='float-end'>
+                      <p className='mb-0 text-center'>First ID Preview </p>
                       <img
                         src={imgFront}
-                        className="rounded p-3 float-end"
+                        className='rounded p-3 float-end'
                         style={{
-                          height: "150px",
-                          width: "150px",
+                          height: '150px',
+                          width: '150px',
                         }}
                       />
                     </div>
                   </div>
                 </div>
 
-                <div className="d-flex align-items-center clearfix">
-                  <Form.Group controlId="formFile" className="w-100 mb-3">
-                    <Form.Label className="text-secondary">
-                      Valid Government Id (Back)
+                <div className='d-flex align-items-center clearfix'>
+                  <Form.Group controlId='formFile' className='w-100 mb-3'>
+                    <Form.Label className='text-secondary'>
+                      Second Valid Government Id
                     </Form.Label>
                     <Form.Control
-                      type="file"
-                      id="formFile"
+                      type='file'
+                      id='formFile'
                       onChange={onImageChangeBack}
                       isInvalid={!!errors.valid_id_back}
                     />
-                    <Form.Control.Feedback type="invalid">
+                    <Form.Control.Feedback type='invalid'>
                       {errors.valid_id_back}
                     </Form.Control.Feedback>
                   </Form.Group>
-                  <div className="w-50">
-                    <div className="float-end">
-                      <p className="mb-0">ID Preview (Back)</p>
+                  <div className='w-50'>
+                    <div className='float-end'>
+                      <p className='mb-0 text-center'>Second ID Preview </p>
                       <img
                         src={imgBack}
-                        className="rounded p-3 float-end"
+                        className='rounded p-3 float-end'
                         style={{
-                          height: "150px",
-                          width: "150px",
+                          height: '150px',
+                          width: '150px',
                         }}
                       />
                     </div>
@@ -253,19 +253,19 @@ export const HomeUser = () => {
                 </div>
 
                 <Form.Group>
-                  <Row xs="auto">
+                  <Row xs='auto'>
                     <Col
                       style={{
-                        padding: "0 0 0 12px",
+                        padding: '0 0 0 12px',
                       }}
                     >
                       <Form.Check
                         checked={check}
                         onChange={handleCheckboxChange}
-                        type="checkbox"
-                        id="termsAndCondition"
+                        type='checkbox'
+                        id='termsAndCondition'
                         isInvalid={!!errors.terms_and_conditions}
-                        feedbackType="invalid"
+                        feedbackType='invalid'
                         feedback={
                           errors.terms_and_conditions &&
                           errors.terms_and_conditions.length > 0 &&
@@ -275,14 +275,14 @@ export const HomeUser = () => {
                     </Col>
                     <Col>
                       <Form.Label
-                        className="text-secondary fw-light mb-0"
-                        controlId="termsAndCondition"
+                        className='text-secondary fw-light mb-0'
+                        controlId='termsAndCondition'
                       >
                         I agree to the
                         <a
                           onClick={handleShow}
-                          target="_blank"
-                          className="fw-bold"
+                          target='_blank'
+                          className='fw-bold'
                         >
                           Terms and Condition
                         </a>
@@ -300,18 +300,18 @@ export const HomeUser = () => {
             </Modal.Body>
             <Modal.Footer>
               <Button
-                variant="secondary"
-                className="rounded"
+                variant='secondary'
+                className='rounded'
                 onClick={closeUpdateAccessModal}
               >
                 Cancel
               </Button>
               <Button
                 disabled={!check}
-                variant="primary"
-                className="rounded"
-                form="updateAccessForm"
-                type="submit"
+                variant='primary'
+                className='rounded'
+                form='updateAccessForm'
+                type='submit'
               >
                 Submit
               </Button>
@@ -328,8 +328,8 @@ export const HomeUser = () => {
             </Modal.Body>
             <Modal.Footer>
               <Button
-                variant="primary"
-                className="rounded"
+                variant='primary'
+                className='rounded'
                 onClick={handleClose}
               >
                 I understand
